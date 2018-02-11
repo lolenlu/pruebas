80b3bbadc7dbfc28fe2a47dea107b447ce2cef45
lolenlu <36380450+lolenlu@users.noreply.github.com>
2018-02-11 20:53:22 +0100

    Update NewMain.java

--- src/NewMain.java
+++ src/NewMain.java
@@ -3,19 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Emilio
  */
 public class NewMain {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("HOLAAA ESTA ES LA PRIMERA MODIFICACION REALIZADA POR LOLENLU");
-        
         System.out.println("HOLAA ESTA ES LA MODIFICACION DEL COLABORADOR DEL PROYECTO LOLENLU2");
     }
     
